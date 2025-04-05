2881d1c2d7243cd489bd0f20a0b94bbc78e449b7
misanthropi-codes <misanthropic.codes@outlook.com>
2025-04-05 22:21:25 +0530

    update v1

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
-console.log(process.env.MONGO_URL);
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // <- Import CORS
 const userRouter = require("./routes/user.routes");
 const investmentRoutes = require("./routes/investment.route");
 const marketRoutes = require("./routes/market.route");
 
 const app = express();
+
+app.use(cors()); // <- Use CORS
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
@@ -15,8 +17,10 @@ app.use("/api/investments", investmentRoutes);
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
-  app.listen(3000);
-  console.log("listening on port 3000");
+  app.listen(8000, () => {
+    console.log("listening on port 8000");
+    console.log("connected to db");
+  });
 }
 
 main();
